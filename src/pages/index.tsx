4e0fbc225866d65141a65b6708fd951eb53d829f
daisuke <dsaito85a@gmail.com>
2022-02-06 22:32:19 +0900

    style: auto format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (<>Hello Gomoku</>)
-}
+  return <>Hello Gomoku</>;
+};
 
-export default Home
+export default Home;
