8ec306699c84a45dfc126cc2dfb4ea3b8a69c55a
daisuke <dsaito85a@gmail.com>
2022-02-13 17:51:31 +0900

    feat: add tsconfig-paths-webpack-plugin and jotai

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, options) => {
+    if (config.resolve.plugins) {
+      config.resolve.plugins.push(new TsconfigPathsPlugin());
+    } else {
+      config.resolve.plugins = [new TsconfigPathsPlugin()];
+    }
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
