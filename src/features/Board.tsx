9c24bb413d8d009057f0a31faa7f559c4cdf12fb
daisuke <dsaito85a@gmail.com>
2022-02-19 14:01:27 +0900

    refactor: use SimpleGrid

--- src/features/Board.tsx
+++ src/features/Board.tsx
@@ -1,4 +1,5 @@
 import { SquareIndex } from "@/type";
+import { SimpleGrid } from "@chakra-ui/react";
 import { Square } from "./Square";
 import { Status } from "./Status";
 
@@ -6,20 +7,17 @@ export const Board = () => {
   return (
     <div>
       <Status />
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "5rem 5rem 5rem",
-          gridTemplateRows: "5rem 5rem 5rem",
-          gridGap: "0.5rem",
-        }}
+      <SimpleGrid
+        gap={"0.5rem 0.5rem"}
+        templateColumns={"5rem 5rem 5rem"}
+        templateRows={"5rem 5rem 5rem"}
       >
         {([0, 1, 2, 3, 4, 5, 6, 7, 8] as SquareIndex[]).map(
           (field: SquareIndex) => (
             <Square key={field} squreIndex={field} />
           )
         )}
-      </div>
+      </SimpleGrid>
     </div>
   );
 };
