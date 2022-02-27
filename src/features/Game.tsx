1961bc0389459caee1596d9625a3dd9f8cba9b75
daisuke <dsaito85a@gmail.com>
2022-02-27 22:32:31 +0900

    refactor: split GameBackground.tsx

--- src/features/Game.tsx
+++ src/features/Game.tsx
@@ -6,6 +6,7 @@ import { Box, Flex, useBoolean, VStack } from "@chakra-ui/react";
 import { useAtom } from "jotai";
 import { useEffect, VFC } from "react";
 import { Board } from "./Board";
+import { GameBackground } from "./GameBackground";
 
 type Props = {
   gameMode: GameMode;
@@ -27,12 +28,7 @@ export const Game: VFC<Props> = ({ gameMode: gameModeProp }) => {
   return (
     <VStack position="relative" h={`calc(100vh - ${headerHeight})`}>
       <Box {...absoluteCenter}>
-        <Box
-          clipPath="polygon(0 0, 100% 0%, 100% 60%, 0% 100%)"
-          backgroundColor="blackAlpha.900"
-          h="55%"
-        />
-        <Box h="45%" />
+        <GameBackground />
       </Box>
       <Flex {...absoluteCenter} alignItems="center" justifyContent="center">
         <Board />
