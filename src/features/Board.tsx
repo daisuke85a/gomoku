e14a60f68828144bf8a90736693d69318db939db
daisuke <dsaito85a@gmail.com>
2022-03-03 10:53:04 +0900

    feat: use goban.jpg and responsive

--- src/features/Board.tsx
+++ src/features/Board.tsx
@@ -1,12 +1,21 @@
 import { getGameModeAtom, selectSquareAtom } from "@/atom";
-import { gameModeConfig } from "@/const";
+import { absoluteCenter, gameModeConfig } from "@/const";
 import { arrayColumnIndex, arrayRowIndex } from "@/game";
-import { Box, Container, ContainerProps, Flex } from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
+import { LayoutProps } from "@chakra-ui/styled-system/src/config/layout";
 import { useAtom, useAtomValue } from "jotai";
 import { VFC } from "react";
 import { Square } from "./Square";
 
-export const Board: VFC<ContainerProps> = ({ ...rest }) => {
+const boardSize: LayoutProps["w"] & LayoutProps["h"] = [
+  "90vw",
+  "70vw",
+  "60vw",
+  "50vw",
+  "40vw",
+];
+
+export const Board: VFC = () => {
   const [selectSquare, setSelectSquare] = useAtom(selectSquareAtom);
   const gameMode = useAtomValue(getGameModeAtom);
 
@@ -15,22 +24,35 @@ export const Board: VFC<ContainerProps> = ({ ...rest }) => {
   }
 
   return (
-    <Container centerContent {...rest}>
-      <Box w="50vw" h="50vw">
-        {arrayRowIndex(gameMode).map((row) => {
-          return (
-            <Flex key={row} h={`calc(100% / ${gameModeConfig[gameMode].rows})`}>
-              {arrayColumnIndex(gameMode).map((column) => (
-                <Square
-                  key={column}
-                  onSelect={() => void setSelectSquare({ row, column })}
-                  state={selectSquare[row]?.[column]}
-                />
-              ))}
-            </Flex>
-          );
-        })}
+    <>
+      <Box w={boardSize} h={boardSize} position="relative">
+        <Image
+          src="/images/goban.jpg"
+          objectFit="fill"
+          height="100%"
+          width="100%"
+          alt="board background"
+          {...absoluteCenter}
+        />
+        <Box {...absoluteCenter}>
+          {arrayRowIndex(gameMode).map((row) => {
+            return (
+              <Flex
+                key={row}
+                h={`calc(100% / ${gameModeConfig[gameMode].rows})`}
+              >
+                {arrayColumnIndex(gameMode).map((column) => (
+                  <Square
+                    key={column}
+                    onSelect={() => void setSelectSquare({ row, column })}
+                    state={selectSquare[row]?.[column]}
+                  />
+                ))}
+              </Flex>
+            );
+          })}
+        </Box>
       </Box>
-    </Container>
+    </>
   );
 };
